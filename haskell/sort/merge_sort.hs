df1c86e38a9ab2318b497ab3f2349706b6b1317d
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-08-29 23:36:12 +0900

    fix

--- haskell/sort/merge_sort.hs
+++ haskell/sort/merge_sort.hs
@@ -9,6 +9,7 @@ mergeSort :: (Ord a) => [a] -> [a]
 mergeSort [] = []
 mergeSort [x] = [x]
 mergeSort xs = let (ys, zs) = halve xs in merge (mergeSort ys) (mergeSort zs)
+               where halve xs = splitAt (length xs `div` 2) xs
 
-halve :: [a] -> ([a], [a])
-halve xs = splitAt (length xs `div` 2) xs
+--halve :: [a] -> ([a], [a])
+--halve xs = splitAt (length xs `div` 2) xs
