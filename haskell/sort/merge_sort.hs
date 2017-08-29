ae4a51efee308c26a5b573a070b15292648e3ab4
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-08-29 23:33:56 +0900

    split

---
+++ haskell/sort/merge_sort.hs
@@ -0,0 +1,14 @@
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+    | x <= y    = x : merge xs (y : ys)
+    | otherwise = y : merge (x : xs) ys
+
+mergeSort :: (Ord a) => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = let (ys, zs) = halve xs in merge (mergeSort ys) (mergeSort zs)
+
+halve :: [a] -> ([a], [a])
+halve xs = splitAt (length xs `div` 2) xs
---
+++ haskell/sort/quick_sort.hs
@@ -0,0 +1,5 @@
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) = let smaller = quickSort [a | a <- xs, a <= x]
+                       larger  = quickSort [a | a <- xs, a > x]
+                   in smaller ++ [x] ++ larger
